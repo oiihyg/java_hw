b431602d7d75e599111d98905b6b4e65904f6b07
oiihyg <oiihyg217@gmail.com>
2024-05-28 17:10:23 +0900

    자바 프로젝트 헬로

---
+++ java_lhh/src/java_lhh_day01/HelloWorld.java
@@ -0,0 +1,18 @@
+package java_lhh_day01;
+
+public class HelloWorld {
+
+
+	/** 메서드 설명을 위한 주석 
+	 * 
+	 */
+	
+	// 프로그램이 실행하기 위해선 main이 필요하다
+	public static void main(String[] args) {
+		
+		//콘솔에 원하는 문구를 출력하는 기능 println
+		System.out.println("HelloWorld \\ dddd");
+		
+	}
+
+}
