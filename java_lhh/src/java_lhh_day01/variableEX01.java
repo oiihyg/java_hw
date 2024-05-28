1c8a18110ea0bfed0fb38eb88464df9a9f55a002
oiihyg <oiihyg217@gmail.com>
2024-05-28 17:10:44 +0900

    자바 프로젝트 기본변수 예제

---
+++ java_lhh/src/java_lhh_day01/variableEX01.java
@@ -0,0 +1,25 @@
+package java_lhh_day01;
+
+public class variableEX01 {
+
+	public static void main(String[] args) {
+
+		char ch1;
+		int num1;
+		
+		ch1 = 'a';
+		num1 = 10;
+		
+		long num2 = 123451111112222111L;// long L추가
+		float num3 = 3.11F;
+		double num4 = 3.11;
+		boolean isEven = true;
+		
+		System.out.println(ch1);
+		System.out.println(num1);
+		System.out.println(ch1);
+		System.out.println(num2);
+		
+	}
+
+}
