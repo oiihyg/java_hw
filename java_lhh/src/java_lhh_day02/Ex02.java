0c8aca4206a58cd3cedf8bca9cd189be715274cb
oiihyg <oiihyg217@gmail.com>
2024-05-29 11:19:21 +0900

    문자 더하기 진행

---
+++ java_lhh/src/java_lhh_day02/Ex02.java
@@ -0,0 +1,16 @@
+package java_lhh_day02;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+
+		int num1 = 1, num2 =2;
+		char ch = '+';
+		
+		String str = "" + num1 + ch + num2;
+		System.out.println("" + num1 + ch + num2);
+		System.out.println(str);
+		
+	}
+
+}
