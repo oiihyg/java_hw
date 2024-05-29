a7e79fe0e3a0dfe2e6c0d9bac88048c0686da596
oiihyg <oiihyg217@gmail.com>
2024-05-29 15:02:53 +0900

    복합 대입 연산자 예제

---
+++ java_lhh/src/java_lhh_day02/AssignmentOperatorEx01.java
@@ -0,0 +1,26 @@
+package java_lhh_day02;
+
+public class AssignmentOperatorEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//복합 대입 연산자 예제
+		// A = A+B
+		// A += B
+		int num1 = 10, num2=20;
+		
+		num1= num1+num2;
+		
+		System.out.println(num1);
+		
+		int num3= 10, num4=20;
+		
+		num3+=num4;
+		
+		System.out.println(num3);
+		
+		
+	}
+
+}
