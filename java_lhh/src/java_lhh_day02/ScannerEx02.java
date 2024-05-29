b59e613bc08e7e0592d3b685e5f533bb737884f4
oiihyg <oiihyg217@gmail.com>
2024-05-29 17:10:29 +0900

    scan 입력 예시 수정

--- java_lhh/src/java_lhh_day02/ScannerEx02.java
+++ java_lhh/src/java_lhh_day02/ScannerEx02.java
@@ -10,16 +10,15 @@ public class ScannerEx02 {
 		/*
 		 * 두 정수와 연산자를 입력 받고 입력받은 결과를 콘솔에 출력
 		 */
-		Scanner scan = new Scanner(System.in); //scan 설정 ctrl+shift+o -> import 자동 설정
+		// Scanner scan 입력 후 ctrl+shift+o -> import 자동 설정
+		Scanner scan = new Scanner(System.in); //scan 설정 
 		
 		
 		int num1 = scan.nextInt(); // 정수 입력
-		int num2 = scan.nextInt(); // 정수 입력
 		String str1 = scan.next(); // 연산자 입력
+		int num2 = scan.nextInt(); // 정수 입력
 		
 		System.out.println(num1+str1+num2); // 출력
-		
-		
 		
 	}
 
