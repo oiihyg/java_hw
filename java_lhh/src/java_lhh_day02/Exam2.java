31912fddd65978a1308545410108db17d04cafc5
oiihyg <oiihyg217@gmail.com>
2024-05-29 12:02:07 +0900

    산술연산자 예제

---
+++ java_lhh/src/java_lhh_day02/Exam2.java
@@ -0,0 +1,20 @@
+package java_lhh_day02;
+
+public class Exam2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 산술 연산자 예제
+		int num1=5, num2=3;
+		System.out.println(""+num1+'+'+num2+'='+(num1+num2));
+		System.out.println(""+num1+'-'+num2+'='+(num1-num2));
+		System.out.println(""+num1+'*'+num2+'='+(num1*num2));
+		System.out.println(""+num1+'/'+num2+'='+(num1/num2)); // 형변환 안한경우
+		System.out.println(""+num1+'/'+num2+'='+(num1/(double)num2)); // 형변환 한경우
+		System.out.println(""+num1+'%'+num2+'='+(num1%num2));
+		
+
+	}
+
+}
