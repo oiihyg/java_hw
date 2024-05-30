397829601bebbdf7c5e71680f81a11eccdf61475
oiihyg <oiihyg217@gmail.com>
2024-05-30 10:42:38 +0900

    IF문 예제2

---
+++ java_lhh/src/java_lhh_day03/IFEx02.java
@@ -0,0 +1,26 @@
+package java_lhh_day03;
+
+import java.util.Scanner;
+
+public class IFEx02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		// if, else 사용~~~~~~~~~
+		Scanner scan = new Scanner(System.in);
+		
+		int num = scan.nextInt();
+		
+		if(num%2==0) {
+			System.out.println("짝수");
+		}else {
+			System.out.println("홀수");
+		}
+		
+		
+		
+	}
+
+}
