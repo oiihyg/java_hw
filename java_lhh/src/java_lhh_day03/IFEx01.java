3e016f4a44ccf03fa0f67342eb57dbfb682034ab
oiihyg <oiihyg217@gmail.com>
2024-05-30 10:35:41 +0900

    if문 예제1

---
+++ java_lhh/src/java_lhh_day03/IFEx01.java
@@ -0,0 +1,24 @@
+package java_lhh_day03;
+
+import java.util.Scanner;
+
+public class IFEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+		
+		int num = scan.nextInt();
+		
+		if(num%2==0) {
+			System.out.println("짝수");
+		}else {
+			System.out.println("홀수");
+		}
+		
+		
+		
+	}
+
+}
