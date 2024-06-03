f7b8b096aa610412f4afe8778c92a9e4c2aa5409
oiihyg <oiihyg217@gmail.com>
2024-06-03 12:11:15 +0900

    별 예제1

---
+++ java_lhh/src/java_lhh_day05/StarEx01.java
@@ -0,0 +1,36 @@
+package java_lhh_day05;
+
+public class StarEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 *  *을 하나씩 출력해서 다음과 같이 출력 되도록 중첩 반복문을 이용하여 작성하세요
+		 *  *****
+		 *  *****
+		 *  *****
+		 *  *****
+		 *  *****
+		 *  반복횟수 : i 는 1부터 5까지 1씩 증가 
+		 *  규칙성 : *****을 출력
+		 *		=> 반복횟수 j는 1부터 5까지 1씩 증가
+		 *		   규칙성 : *을 출력(한줄로)
+		 *		   반복문 종료 후 : 엔터를 출력
+		 *	반복문 종료 후 : 없음
+		 */
+		int i,j;
+		char chr = '*';
+		for(i=1;i<6;i++) {
+			for(j=1;j<6;j++) {
+				System.out.print(chr);
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+	}
+
+}
