c84a1687050463ceb0c1329ea799ef4697aa1c02
oiihyg <oiihyg217@gmail.com>
2024-06-04 16:50:14 +0900

    Create ArrayEx9.java

---
+++ java_lhh/src/java_lhh_day06/ArrayEx9.java
@@ -0,0 +1,20 @@
+package java_lhh_day06;
+
+import java.util.Scanner;
+
+public class ArrayEx9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String [] list = {"dog","cat","java","cup","computer"};
+		
+		/*
+		 * 문자열을 입력받아 입력받은 문자열이 있는지 없는지 알려주는 코드 작성
+		 */
+	
+		
+		
+	}
+
+}
