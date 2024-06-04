1b07430b1716a35d7ed6b8a335100c2446bf6389
oiihyg <oiihyg217@gmail.com>
2024-06-04 11:26:15 +0900

    Create ArrayEx01.java

---
+++ java_lhh/src/java_lhh_day06/ArrayEx01.java
@@ -0,0 +1,18 @@
+package java_lhh_day06;
+
+public class ArrayEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[]arr1, arr2;
+		int arr3 [],arr4;
+		
+		arr1 = new int[3];
+		arr2 = new int[3];
+		arr3 = new int[3];
+		// arr4 = new int[3]; // 배열이 아니다~ 위에 변수 선언에서 배열로 선정 안함
+		arr4 = 10; // arr4는 변수~ 
+	}
+
+}
