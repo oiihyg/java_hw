802b277a0f753956bca2f0f807ab623460c6545d
oiihyg <oiihyg217@gmail.com>
2024-06-04 17:09:59 +0900

    Update ArrayEx9.java

--- java_lhh/src/java_lhh_day06/ArrayEx9.java
+++ java_lhh/src/java_lhh_day06/ArrayEx9.java
@@ -12,7 +12,14 @@ public class ArrayEx9 {
 		/*
 		 * 문자열을 입력받아 입력받은 문자열이 있는지 없는지 알려주는 코드 작성
 		 */
-	
+		
+		// 입력 스캔 준비 완료! 
+		Scanner scan = new Scanner(System.in);
+		
+		// 문자열 입력 받기
+		
+		// 문자열이 있는지 없는지 확인하기
+		
 		
 		
 	}
