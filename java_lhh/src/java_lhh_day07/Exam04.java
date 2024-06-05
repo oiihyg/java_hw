e0b43ae4c746e01dfee8eae6cbe2da3acfc4c604
oiihyg <oiihyg217@gmail.com>
2024-06-05 16:44:24 +0900

    Create Exam04.java

---
+++ java_lhh/src/java_lhh_day07/Exam04.java
@@ -0,0 +1,31 @@
+package java_lhh_day07;
+
+public class Exam04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * Exam01~03 기능을 하나로 합쳐서 다음 기능을 하나로 출력하세요
+		 * 메뉴
+		 * 1. 추가
+		 * 2. 검색
+		 * 3. 종료
+		 * 1 => 단어 입력 (-1): abad
+		 * 		단어를 추가했습니다. / 단어를 추가하지 못했습니다. 
+		 * 		-1 입력 시 메뉴로 돌아갑니다.
+		 * 2 => 단어 입력 (-1): abad
+		 * 		있는 단어입니다. / 없는 단어입니다.
+		 * 3 => 프로그램을 종료합니다.
+		 * 4 => 잘못된 메뉴입니다.
+		 */
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
