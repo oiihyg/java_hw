9dde99c3cc9753ea2dc237e54a50c28332abe6b2
oiihyg <oiihyg217@gmail.com>
2024-06-13 17:32:25 +0900

    Create WrapperEx01.java

---
+++ java_lhh/src/java_lhh_day12/WrapperEx01.java
@@ -0,0 +1,18 @@
+package java_lhh_day12;
+
+public class WrapperEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str1 = "123";
+		int num = Integer.parseInt(str1);
+		System.out.println(num);
+		
+		String str2 = "3.14";
+		double num2 = Double.parseDouble(str2);
+		System.out.println(num2);
+		
+	}
+
+}
