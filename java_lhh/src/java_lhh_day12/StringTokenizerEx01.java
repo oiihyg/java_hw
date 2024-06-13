790d6041ac43b3905f432d076042d08270c29b17
oiihyg <oiihyg217@gmail.com>
2024-06-13 15:40:43 +0900

    Create StringTokenizerEx01.java

---
+++ java_lhh/src/java_lhh_day12/StringTokenizerEx01.java
@@ -0,0 +1,22 @@
+package java_lhh_day12;
+
+import java.util.StringTokenizer;
+
+public class StringTokenizerEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// StringTokenizer : 문자열이 한 종류의 구분자로 되어 있는 경우 토큰을 쉽게 분리할 수 있게 하는 클래스
+		String fruit = "사과,포도,배";
+		StringTokenizer st = new StringTokenizer(fruit, ",");
+		// hasMoreTokens() : 토큰이 더 있는지를 알려주는 메소드
+		while(st.hasMoreTokens()) {
+			String token = st.nextToken();
+			System.out.println(token);
+		}
+		
+		
+	}
+
+}
