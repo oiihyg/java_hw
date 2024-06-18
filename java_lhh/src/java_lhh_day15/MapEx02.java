df7d6b7db858ff01b9e0a108d275a19a249eb277
oiihyg <oiihyg217@gmail.com>
2024-06-18 16:49:20 +0900

    Create MapEx02.java

---
+++ java_lhh/src/java_lhh_day15/MapEx02.java
@@ -0,0 +1,45 @@
+package java_lhh_day15;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class MapEx02 {
+
+	public static void main(String[] args) {
+
+		HashMap<String, String> map = new HashMap<String, String>();
+		
+		map.put("202416001", "고길동");
+		map.put("202416002", "홍길동");
+		map.put("202413501", "임꺽정");
+		
+		// 방법1. keySet을 이용하여 set으로 만든 후 set을 반복문을 이용하여 원하는 작업
+		Set<String> set = map.keySet();
+		
+		Iterator<String> it = set.iterator();
+		
+		while(it.hasNext()) {
+			String key = it.next();
+			String value = map.get(key);
+			System.out.println(key+" : "+value);
+		}
+		
+		
+		// 방법2. EntrySet을 이용.Entry 클래스를 이용하여 set을로 만들고, 반복문을 활용
+		Set<Map.Entry<String, String>> entrySet = map.entrySet();
+		
+		Iterator<Map.Entry<String, String>> it2 = entrySet.iterator();
+		
+		while(it2.hasNext()) {
+			Map.Entry<String, String> tmp = it2.next();
+			String key = tmp.getKey();
+			String value = tmp.getValue();
+			System.out.println(key+" : "+value);
+		}
+		
+		
+	}
+
+}
