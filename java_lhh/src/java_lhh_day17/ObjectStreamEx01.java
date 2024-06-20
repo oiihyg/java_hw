8d42468389304c75228da54ccd800e8dcd07c87f
oiihyg <oiihyg217@gmail.com>
2024-06-20 16:04:47 +0900

    Create ObjectStreamEx01.java

---
+++ java_lhh/src/java_lhh_day17/ObjectStreamEx01.java
@@ -0,0 +1,16 @@
+package java_lhh_day17;
+
+public class ObjectStreamEx01 {
+
+	public static void main(String[] args) {
+
+		String fileName = "src/java_lhh_day17/objectStream.txt";
+		//try(){
+			
+		//}
+		
+		
+	}
+
+}
+
