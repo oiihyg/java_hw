03460843fb07c3d21d2b61a935d0d5054f20f1dd
oiihyg <oiihyg217@gmail.com>
2024-06-20 12:34:30 +0900

    Create BufferedStreamEx01.java

---
+++ java_lhh/src/java_lhh_day17/BufferedStreamEx01.java
@@ -0,0 +1,32 @@
+package java_lhh_day17;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedStreamEx01 {
+
+	public static void main(String[] args) {
+
+		String fileName = "src/java_lhh_day17/charStream.txt";
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
+			String str;
+			/*
+			do {
+				str = br.readLine();
+				System.out.println(str);
+			}while(str != null);
+			*/
+			while((str = br.readLine()) != null) {
+				System.out.println(str);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("파일이 없거나 파일을 생성할 수 없습니다.");
+		} catch (IOException e1) {
+			System.out.println("예외가 발생했습니다.");
+		}		
+	}
+
+}
