b7c797cae954072366f41d041026628aedd3290f
oiihyg <oiihyg217@gmail.com>
2024-06-20 16:04:33 +0900

    Create ByteStreamEx02.java

---
+++ java_lhh/src/java_lhh_day17/ByteStreamEx02.java
@@ -0,0 +1,22 @@
+package java_lhh_day17;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ByteStreamEx02 {
+
+	public static void main(String[] args) {
+		
+		try(FileOutputStream fos = new FileOutputStream("src/java_lhh_day17/byteStream3.txt")) {
+			for(char ch ='A'; ch <= 'Z'; ch++) {
+				fos.write(ch);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("파일이 없거나 파일을 생성할 수 없습니다.");
+		} catch (IOException e1) {
+			System.out.println("예외가 발생했습니다.");
+		}
+	}
+
+}
