6ef4d814d6bc725ce184d80e585f0579aaab4167
oiihyg <oiihyg217@gmail.com>
2024-06-25 16:30:03 +0900

    Update Schedule.java

--- java_lhh/src/java_lhh_day18/homeV1/Schedule.java
+++ java_lhh/src/java_lhh_day18/homeV1/Schedule.java
@@ -8,7 +8,9 @@ import lombok.Data;
 //Collections.sort()를 사용하기 위해 Comparable 인터페이스를 구현
 //객체로 저장하기 위해 Serializable 인터페이스 구현
 public class Schedule implements Comparable<Schedule>, Serializable{
-	private static final long serialVersionUID = 1L;
+	
+	private static final long serialVersionUID = 12345L;
+	
 	private String date;//날짜
 	private String toDo;//할일
 	private String detail;//상세
