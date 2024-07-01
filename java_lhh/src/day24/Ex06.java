feaf4ba1aace4ce1b29ddfae2b552524948787df
oiihyg <oiihyg217@gmail.com>
2024-07-01 17:02:21 +0900

    Create Ex06.java

---
+++ java_lhh/src/day24/Ex06.java
@@ -0,0 +1,19 @@
+package day24;
+
+public class Ex06 {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * 1부터 10까지 출력하는 코드를 작성
+		 * 원인 : i=0, 증감식
+		 * 해결 : i=1, i++
+		 * 
+		 */
+		for(int i=0;i<=10;) {
+			System.out.println(i);
+		}
+		
+	}
+	
+}
