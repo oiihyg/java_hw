65560a9b4b196d2b01cd3852232167f839e6e453
oiihyg <oiihyg217@gmail.com>
2024-07-01 17:02:22 +0900

    Create Ex07.java

---
+++ java_lhh/src/day24/Ex07.java
@@ -0,0 +1,21 @@
+package day24;
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 1부터 10까지의 합을 구하시오
+		 * 원인 : for ;
+		 * 해결 : ; 삭제
+		 */
+		
+		int i;
+		int sum = 0;
+		for(i=1;i<=10;i++);{
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+
+}
