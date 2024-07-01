cd1b73c9bb1ed2ebe83b4e87cd519225d7bd18b4
oiihyg <oiihyg217@gmail.com>
2024-07-01 17:02:35 +0900

    Create Ex15.java

---
+++ java_lhh/src/day24/Ex15.java
@@ -0,0 +1,39 @@
+package day24;
+import java_lhh_program.Program;
+
+public class Ex15 {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
+
+/*
+ * Program 인터페이스를 구현한 구현 클래스를 만들려고 했다
+ * 원인 : 
+ * 해결 : 
+ */
+class TestProgram implements Program{
+
+	@Override
+	public void printMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void runMenu(int menu) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
