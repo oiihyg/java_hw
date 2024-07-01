4f91fef6a602d7ce29bc5817b6548bb402a0384d
oiihyg <oiihyg217@gmail.com>
2024-07-01 17:02:12 +0900

    Create Main.java

---
+++ java_lhh/src/account/v2/Main.java
@@ -0,0 +1,14 @@
+package account.v2;
+
+import java.text.ParseException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AccountManager am = new AccountManager();
+		am.run();
+
+	}
+
+
+}
