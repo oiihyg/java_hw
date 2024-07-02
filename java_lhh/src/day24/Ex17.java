8805da33be6954233b460819c35909d90adb28f0
oiihyg <oiihyg217@gmail.com>
2024-07-02 17:21:21 +0900

    Update Ex17.java

--- java_lhh/src/day24/Ex17.java
+++ java_lhh/src/day24/Ex17.java
@@ -1,5 +1,6 @@
 package day24;
 
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -9,11 +10,12 @@ public class Ex17 {
 		/*
 		 * 파일을 이어 쓰려면 뒤에 true를 추가해주면 된다
 		 */
-		try (FileWriter fw = new FileWriter("src/day24/ex17.txt", true)){
-			fw.write('a');
-			fw.write('b');
-			fw.write('c');
+		try (FileWriter fw = new FileWriter("src/day24/ex17.txt")){
+			fw.write(97);
+			fw.write(65);
 			fw.flush();
+		}catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}catch (IOException e) {
 			e.printStackTrace();
 		}
